b255a471dc1b7cadea5d4f7b68ce4485a8b73d3f
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-10-18 14:15:24 +0000

    Build system improvements

--- {{cookiecutter.project_slug}}/scripts/account_id.py
+++ {{cookiecutter.project_slug}}/scripts/account_id.py
@@ -5,4 +5,5 @@ with open(os.path.join(slug, 'graphs', slug, 'graph.json')) as f:
     graph = json.load(f)
 for package in graph['nodeGraph']['packages']:
     if package['name'].endswith(package_name):
-	    print(package['name'].split('::')[0])
+        print(package['name'].split('::')[0])
+        break
