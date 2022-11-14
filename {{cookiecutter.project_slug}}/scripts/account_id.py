dbc6124a197f23bb002e535d29eb0ecdfb06f5e4
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-11-14 15:35:30 +0100

    Bugfix in account_id script

--- {{cookiecutter.project_slug}}/scripts/account_id.py
+++ {{cookiecutter.project_slug}}/scripts/account_id.py
@@ -1,6 +1,6 @@
 import json, os
-slug = 'panorama_video_processor'
-package_name = 'panorama_video_processor_logic'
+slug = '{{cookiecutter.project_slug}}'
+package_name = '{{cookiecutter.code_package_name}}'
 with open(os.path.join(slug, 'graphs', slug, 'graph.json')) as f:
     graph = json.load(f)
 for package in graph['nodeGraph']['packages']:
