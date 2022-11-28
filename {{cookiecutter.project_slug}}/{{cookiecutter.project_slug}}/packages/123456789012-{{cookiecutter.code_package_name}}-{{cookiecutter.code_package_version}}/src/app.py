ffc505a3432733f5bcda74032779277a6acdba73
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2022-11-28 11:30:05 -0800

    Fix comments in app.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/packages/123456789012-{{cookiecutter.code_package_name}}-{{cookiecutter.code_package_version}}/src/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/packages/123456789012-{{cookiecutter.code_package_name}}-{{cookiecutter.code_package_version}}/src/app.py
@@ -31,13 +31,10 @@ logger = get_logger(level=logging.INFO, test_utility=TEST_UTILITY_ENVIRONMENT)
 
 class Application(panoramasdk.node):
 
-    MODEL_NODE_NAME: str = '{{ cookiecutter.model_node_name }}'
     MODEL_NODE_NAME: str = (
         '{{ cookiecutter.model_asset_name }}' if TEST_UTILITY_ENVIRONMENT else
         '{{ cookiecutter.model_node_name }}'
-    )
-    # The test utility erroneously wants the model asset name, meanwhile the real environment
-    # expects the model node name
+    ) # The test utility erroneously wants the model asset name instead of model node name
     MODEL_INPUT_NAME: str = '{{ cookiecutter.model_input_name }}'
     MODEL_INPUT_WIDTH: int = int('{{ cookiecutter.model_processing_width }}')
     MODEL_INPUT_HEIGHT: int = int('{{ cookiecutter.model_processing_height }}')
